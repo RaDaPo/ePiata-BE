9a2eab8ab46cfda219f492c9e7f5aa41f7b4877f
Lucian Cristea <luciancristea1997@gmail.com>
2019-04-05 23:27:47 +0300

    fix

---
+++ src/main/java/ro/hacktm/oradea/epiata/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ro.hacktm.oradea.epiata.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import ro.hacktm.oradea.epiata.model.entity.User;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    List<User> findAll();
+}
