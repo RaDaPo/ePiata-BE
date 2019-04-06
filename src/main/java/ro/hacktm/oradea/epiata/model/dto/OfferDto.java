f99cb1327ea82b5563cdb63809c80e01425bcc6c
Lucian Cristea <luciancristea1997@gmail.com>
2019-04-06 16:09:05 +0300

    fix

--- src/main/java/ro/hacktm/oradea/epiata/model/dto/OfferDto.java
+++ src/main/java/ro/hacktm/oradea/epiata/model/dto/OfferDto.java
@@ -2,12 +2,16 @@ package ro.hacktm.oradea.epiata.model.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class OfferDto {
-	private String name;
-	private String owner;
-	private String startDate;
-	private String endDate;
-	private String imgLocation;
-	private String description;
+    private String name;
+    private String owner;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+    private Long id;
+    private String quantity;
+    private String price;
 }
--- src/main/java/ro/hacktm/oradea/epiata/model/entity/OfferDao.java
+++ src/main/java/ro/hacktm/oradea/epiata/model/entity/OfferDao.java
@@ -11,35 +11,37 @@ import java.util.Date;
 @Data
 public class OfferDao {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String name;
-	private String owner;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String owner;
+    private String quantity;
+    private String price;
 
-	@JoinColumn
-	private Long category;
+    @JoinColumn
+    private Long category;
 
-	@Column(name = "start_date")
-	private Date startDate;
-	@Column(name = "end_date")
-	private Date endDate;
-	@Column(name = "img_link")
-	private String imgLocation;
-	private String description;
-	@ManyToOne()
-	@JoinColumn(name = "user_id")
-	private UserDao user;
+    @Column(name = "start_date")
+    private Date startDate;
+    @Column(name = "end_date")
+    private Date endDate;
+    private String description;
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private UserDao user;
 
-	public OfferDto toDto() {
-		OfferDto dto = new OfferDto();
-		dto.setDescription(this.getDescription());
-		dto.setEndDate(this.getEndDate().toString());
-		dto.setImgLocation(this.getImgLocation());
-		dto.setName(this.getName());
-		dto.setStartDate(this.getStartDate().toString());
-		dto.setOwner(this.getOwner());
-		return dto;
-	}
+    public OfferDto toDto() {
+        OfferDto dto = new OfferDto();
+        dto.setDescription(this.getDescription());
+        dto.setEndDate(this.getEndDate());
+        dto.setName(this.getName());
+        dto.setStartDate(this.getStartDate());
+        dto.setOwner(this.getOwner());
+        dto.setPrice(this.getPrice());
+        dto.setQuantity(this.getQuantity());
+        dto.setId(this.getId());
+        return dto;
+    }
 
 }
